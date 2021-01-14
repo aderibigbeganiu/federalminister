b04d941c67fd855b9844f3288d890b404c708540
aderibigbeganiu <aderibigbeganiu@gmail.com>
2021-01-14 13:42:11 +0100

    make it better

--- ministry/settings.py
+++ ministry/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'ministry/static'),
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
