1659baf28ac43372efd0c1f5d49f37d0f3e17879
aderibigbeganiu <aderibigbeganiu@gmail.com>
2021-01-14 15:43:25 +0100

    make it better

--- ministries/models.py
+++ ministries/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Ministries(models.Model):
     name = models.CharField(max_length=200)
     minister = models.CharField(max_length=100)
+    minister_state = models.CharField(max_length=100)
     origin = models.CharField(max_length=100)
     address = models.CharField(max_length=500)
     website = models.URLField(max_length=200)
